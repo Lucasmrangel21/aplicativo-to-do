c3313076ed5d4be55dfe3001de9ccb8701c820da
lucas martins <lucas.mrangel@sempreceub.com>
2022-06-24 16:14:43 -0300

    Finalizando projeto

--- src/app/modules/home/componenents/todo-list/todo-list.component.ts
+++ src/app/modules/home/componenents/todo-list/todo-list.component.ts
@@ -10,16 +10,13 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent implements DoCheck {
 
-  public taskList: Array<TaskList> = [
-    //{task: "Minha nova Task", checked: true},
-    //{task: "Minha nova Task", checked: false},
-
-  ];
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
 
   constructor() { }
 
   ngDoCheck(){
-    this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+    this.setLocalStorage();
+
   }
 
   public setEmitTaskList(event: string){
@@ -46,6 +43,13 @@ export class TodoListComponent implements DoCheck {
       if(confirm){
         this.deleteItemTaskList(index);
       }
+    }
+  }
+
+  public setLocalStorage(){
+    if(this.taskList){
+      this.taskList.sort((first, last) => Number(first.checked) - Number(last.checked));
+      localStorage.setItem("list", JSON.stringify(this.taskList));
     }
   }
 
